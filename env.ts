9db31d8e0b4dd7b2ef018a66b8739ff96c900a7e
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 15:48:38 +0200

    chore: update

---
+++ env.ts
@@ -0,0 +1,6 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: { domain: 'tnp-models.example.domain.com', title: 'Tnp Models' },
+};
+export default env;
